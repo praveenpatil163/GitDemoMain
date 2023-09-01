fef2e278a074c872ec49050fc3e03f1b83862e72
praveenpatil163 <praveenpatil163@gmail.com>
2023-09-01 13:12:12 +0530

    Updated commit By Git workspace

---
+++ src/pageObjectModel/GitPractice.java
@@ -0,0 +1,12 @@
+package pageObjectModel;
+
+public class GitPractice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HIiii");
+		System.out.println("Practicing git functionalities");
+
+	}
+
+}
